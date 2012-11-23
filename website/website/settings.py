cb8f4cc0cd753b386fdb38faa057cf33596c9275
chromer <chromer@in.com>
2012-11-23 12:36:41 +0530

    Add email backend to login using email.

--- website/website/settings.py
+++ website/website/settings.py
@@ -1,5 +1,9 @@
 # Django settings for website project.
 
+import os
+
+DIRNAME = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -106,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(DIRNAME, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -152,6 +157,11 @@ LOGGING = {
         },
     }
 }
+
+AUTHENTICATION_BACKENDS = (
+    'accounts.email_backend.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 
